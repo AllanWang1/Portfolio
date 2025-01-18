2adaca569b94d44d5b92ccd6b147d9d929cb0148
allanwang <allan357161@gmail.com>
2025-01-18 03:51:06 -0800

    Added react-router-dom features to switch between webpages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import About from "./Components/About/About";
@@ -6,17 +8,29 @@ import Experiences from "./Components/Experiences/Experiences";
 import Projects from "./Components/Projects/Projects";
 import Contact from "./Components/Contact/Contact";
 import More from "./Components/More/More";
+
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <About />
-      <Experiences />
-      <Projects />
-      <More />
-      <Contact />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/Portfolio-Vite/"
+          element={
+            <div>
+              <Navbar />
+              <Hero />
+              <About />
+              <Experiences />
+              <Projects />
+              <More />
+              <Link to="/cubing">Cubing Page</Link>
+              <Contact />
+            </div>
+          }
+        ></Route>
+        <Route path="/cubing" element={<Projects />}></Route>
+      </Routes>
+    </Router>
   );
 };
 
