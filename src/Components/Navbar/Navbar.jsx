5def8d3b7d297058c9e2c3b8b10b64a012f42b44
allanwang <allan357161@gmail.com>
2025-04-17 11:11:21 -0700

    UI and Resume update.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
 import "./Navbar.css";
-import underline from "../../assets/nav_underline.svg";
+import underline from "/AW.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import menu_open from "../../assets/menu_open.svg";
 import menu_close from "../../assets/menu_close.svg";
