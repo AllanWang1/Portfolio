41c55abee431fcf32ff45313ca4c36d408b0a814
allanwang <allan357161@gmail.com>
2026-09-20 12:42:59 -0700

    Upload new video.

--- src/Components/More/More.jsx
+++ src/Components/More/More.jsx
@@ -1,9 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
 import "./More.css";
 import Cubing from "./Cubing/Cubing";
 import Badminton from "../../assets/badminton.mp4";
 import Badminton2 from "../../assets/badminton2.mp4";
+import Badminton3 from "../../assets/badminton3.mp4";
 
 const More = () => {
   return (
@@ -14,6 +13,7 @@ const More = () => {
       <Cubing />
       <div className="badminton">
         <h3>Badminton</h3>
+        <video src={Badminton3} autoPlay muted loop playsInline />
         <video src={Badminton} autoPlay muted loop playsInline />
         <video src={Badminton2} autoPlay muted loop playsInline />
       </div>
